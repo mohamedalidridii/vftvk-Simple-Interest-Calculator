2a0235827c95851c6d2b3dd00af961057999e519
mohamedalidridii <96180127+mohamedalidridii@users.noreply.github.com>
2022-03-04 12:28:29 +0100

    Those files for submit my project (IBM)
    This is an interest calculator

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    var Amount = document.getElementById("principal").value;
+    var Rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var result = Amount*Rate/100*years;
+    document.getElementById("result").innerHTML=( "If you deposit " + Amount + "," + " at an interest rate of " + Rate +"%," + " You will receive an amount of " +  result);
+
+
     
 }
-        
+function updateTextInput(val) {
+    document.getElementById('textInput').value=(val+"%"); 
+  }    
